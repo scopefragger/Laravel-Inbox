d347dd272cb41851090406455635e3780667fa2b
Mark A Jones <mark@bmkdigital.co.uk>
2017-06-13 18:15:22 +0100

    First Blood

---
+++ src/Config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enabled
+    |--------------------------------------------------------------------------
+    |
+    | Allows you to quickly disbale the package in the event of a compleate disaster
+    | Accepts ( Bool ) : true | false
+    |
+    */
+
+    'enabled' => env(INBOX_ENABLED, true),
+
+];
---
+++ src/LaravelInboxServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Scopefragger\LaravelInbox;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelInboxServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes.php');
+        $this->loadMigrationsFrom(__DIR__ . '/Migrations');
+        $this->publishes([
+            __DIR__.'/Config/config.php' => config_path('laravel-inbox.php'),
+        ],'laravel-inbox');
+    }
+}
